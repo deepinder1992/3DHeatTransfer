6b19bb28a8bae3a9f6788780e8435ee199701b20
deepinder1992 <deepinderjot@gmail.com>
2026-02-20 02:09:58 -0700

    Noexecpt

--- include/boundaryConditions.hpp
+++ include/boundaryConditions.hpp
@@ -7,8 +7,8 @@ class BoundaryConditions{
     public:
         BoundaryConditions(std::array<BCType,6>types, std::array<double,6>values);
         
-        const std::array<BCType,6>& types() const {return types_;}
-        const std::array<double,6>& values() const {return values_;}
+        std::array<BCType,6> types() const noexcept {return types_;}
+        std::array<double,6> values() const noexcept {return values_;}
         
         void applyBCsToStencil(Grid3D& grid, double dx, double cond) const;
         void applyBCsToRhsMatrix(size_type nx, size_type ny, size_type nz, double dx, double cond, double coeff, std::vector<double>& b) const;
